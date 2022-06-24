5184d0c35274674172391a1115622954ea7ea164
Ahmed <ahmed.al-refaie.12@ucl.ac.uk>
2022-06-24 13:57:52 +0100

    Implemented doppler profile

--- picocross/profiles.py
+++ picocross/profiles.py
@@ -26,3 +26,34 @@ def doppler_hwhm(
     left_factor = np.sqrt(2 * const.k_B * np.log(2) * temperature / molecular_mass)
 
     return left_factor * transition_fixed / const.c
+
+
+def doppler_profile(
+    bins: u.Quantity, transitions: u.Quantity, doppler_hwhm: u.Quantity
+) -> u.Quantity:
+    """Compute doppler profile using the form:
+
+    .. math::
+        f = g(x)
+
+    Where :math:`g(x)` is the doppler profile.
+
+    Args:
+        bins: Frequency bins to compute the profile
+        transitions: Transitions to compute the profile
+        doppler_hwhm: Doppler HWHM (Compute from :func:`~doppler_hwhm`)
+
+    Returns:
+        Computed doppler profile
+
+    """
+
+    v_squared = (bins[None, :] - transitions[:, None]) ** 2
+
+    ln2 = np.log(2)
+
+    doppler_fixed = doppler_hwhm[:, None]
+
+    left_side = np.sqrt(ln2 / np.pi) / doppler_fixed
+
+    return left_side * np.exp(-v_squared * ln2 / doppler_fixed**2)
--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -3,6 +3,8 @@ import pytest
 from astropy import constants as const
 from astropy import units as u
 
+from picocross.profiles import doppler_hwhm
+
 
 @pytest.mark.parametrize(
     "unit",
@@ -20,3 +22,19 @@ def test_doppler_hwhm(unit):
     mass = 12 * u.g / u.mol / const.N_A
 
     doppler_hwhm(trans, 1000 * u.K, mass).to(1 / u.m)
+
+
+def test_doppler_profile():
+    from picocross.profiles import doppler_profile
+
+    trans = np.linspace(100, 1000, 10) * u.k
+
+    doppler_hwhm = np.linspace(2, 10, 10) / u.cm
+
+    bins = np.linspace(300, 600, 20) * u.k
+
+    result = doppler_profile(bins, trans, doppler_hwhm)
+
+    assert result.shape == (10, 20)
+
+    result.to(u.cm)
